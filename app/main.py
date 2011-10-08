edb066b04d0e504910923331efdff45e7673f78b
d1ffuz0r <gladk0w@mail.ru>
2011-10-09 00:00:23 +0400

    replace tabs's on whitespace's

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os	
+import os    
 from re import findall
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
@@ -9,28 +9,28 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 class Last(object):
-	def getLibrary(self,username=''):
-		total = fetch("http://ws.audioscrobbler.com/2.0/?method=library.getartists&api_key=b25b959554ed76058ac220b7b2e0a026&limit=9000&user=%s" % username).content
-		return ', '.join(findall(r'<name>(.*?)</name>',total)).decode('utf-8')
+    def getLibrary(self,username=''):
+        total = fetch("http://ws.audioscrobbler.com/2.0/?method=library.getartists&api_key=b25b959554ed76058ac220b7b2e0a026&limit=9000&user=%s" % username).content
+        return ', '.join(findall(r'<name>(.*?)</name>',total)).decode('utf-8')
 
 class MainPage(webapp.RequestHandler):
     def get(self):
         path = os.path.join(os.path.dirname(__file__), 'index.html')
-    	self.response.out.write(template.render(path,()))
+        self.response.out.write(template.render(path,()))
 
 class Get(webapp.RequestHandler, Last):
     def post(self):
-    	if self.request.get('username'):
-    		out = self.getLibrary(self.request.get('username'))
+        if self.request.get('username'):
+            out = self.getLibrary(self.request.get('username'))
         else:
-        	out = '<h1>Введите имя учётной записи на Last.FM</h1>'
-    	self.response.out.write(out)
+            out = '<h1>Введите имя учётной записи на Last.FM</h1>'
+        self.response.out.write(out)
 
 application = webapp.WSGIApplication([
-									('/', MainPage),
-									('/get', Get)],
-									debug=False
-									)
+                                    ('/', MainPage),
+                                    ('/get', Get)],
+                                    debug=False
+                                    )
 
 def main():
     run_wsgi_app(application)
